73f12a15f63e061a6c8f1ebe3e05324f76e62526
yusufuysal <yusuf.uysal@agu.edu.tr>
2024-03-22 00:38:31 +0300

    Give hover effect to sidebar button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ const Sidebar = () => {
     <div className=" fixed bottom-0 left-0 w-1/3 max-w-96 h-[95vh] bg-zinc-950 rounded-se-xl text-white">
         <div className=" pt-16 pl-8 flex flex-col gap-8">
             <h3 className=" uppercase text-slate-200">Your Projects</h3>
-            <button className=" bg-stone-800 w-32 p-2 rounded-md flex items-center text-stone-400">
+            <button className=" bg-stone-800 w-32 p-2 rounded-md flex items-center text-stone-400 hover:bg-stone-700">
                 <svg class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
                 </svg>
