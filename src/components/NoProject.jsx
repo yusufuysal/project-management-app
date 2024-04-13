141c73aa86018d778ced08ea34ea54c9a82cc033
yusufuysal <yusuf.uysal@agu.edu.tr>
2024-04-13 18:27:36 +0300

    Give functionality to create new project button

--- src/components/NoProject.jsx
+++ src/components/NoProject.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { ProjectsContext } from "../store/projects-context";
 import img from "../assets/no-projects.png";
 
 const NoProject = () => {
+  const { handleOpenAddForm } = useContext(ProjectsContext);
+
   return (
     <div className="flex flex-col gap-4 justify-center items-center pb-24 w-10/12">
       <img className="h-20" src={img} alt="no projects" />
@@ -10,7 +14,10 @@ const NoProject = () => {
       <p className="text-stone-500">
         Select a project or get started with a new one
       </p>
-      <button className=" bg-stone-700 text-stone-400 py-2 px-4 rounded-md mt-6 hover:bg-stone-800">
+      <button
+        onClick={handleOpenAddForm}
+        className=" bg-stone-700 text-stone-400 py-2 px-4 rounded-md mt-6 hover:bg-stone-800"
+      >
         Create new project
       </button>
     </div>
